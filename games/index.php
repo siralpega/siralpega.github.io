cc132aaf29181470f60a00c1a6bc5f7d3dfc0211
siralpega <6191199+siralpega@users.noreply.github.com>
2020-08-30 21:52:22 -0400

    Add files via upload

---
+++ games/index.php
@@ -0,0 +1,14 @@
+<?php
+	require "./games-header.php";
+?>
+<!-- Only need closing php tag if you are ading HTML code in the file. if pure php, no need -->
+	<main>
+	<!-- TODO: add box layout, with buttons for the links w/ a image and short descp in box -->
+	<p>Asteroid Clicker			<a href="https://alpega.itch.io/asteroid-clicker">(webgl)</a></p>
+	<br/>
+	<p>Platformer			<a href="../dls/platform.exe">(win)</a>   <a href="">(webgl)</a></p>
+	</main>
+
+<?php
+	require "../footer.php";
+?>
